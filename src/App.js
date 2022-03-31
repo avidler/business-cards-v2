b68121a7ee98c2fb302678a67885449f17cfb25f
avidler <a.vidler@live.co.uk>
2022-03-31 22:33:39 +0100

    Created json file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@ import './App.css';
 import BusinessCard from './components/BusinessCard.jsx'
 
 function App() {
+  fetch("./data.json")
+  .then(res => res.json())
+  .then(data => {
+    console.log(data)
+      document.getElementById("person-name").textContent = data.name
+      document.getElementById("person-role").textContent = data.role
+      document.getElementById("person-website").textContent = data.website
+      document.getElementById("person-about").textContent = data.about
+      document.getElementById("person-interests").textContent = data.interests
+  })
+  
+  .catch(function (err) {
+    console.log("Fetch problem show: " + err.message);
+  });
   return (
     <div className="App">
       <BusinessCard />
--- src/components/BusinessCard.jsx
+++ src/components/BusinessCard.jsx
@@ -5,15 +5,19 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 const element = <FontAwesomeIcon icon={faEnvelope} />
 
+
+
 export default function BusinessCard() {
+
     return (
-        <div id="business-card" class="font-sans bg-gray-800 text-white w-[550px] h-[868px] flex flex-col">
+        
+        <div id="business-card" className="font-sans bg-gray-800 text-white w-[550px] h-[868px] flex flex-col">
             <img src={image} class="object-cover object-top w-[317px] h-[317px] mx-auto" alt="face" />
             <div class="bg-gray-900 w-[317px] mx-auto"> 
                 <div id="name" class="w-[247px] mx-auto pt-6">
-                    <p class="not-italic font-bold text-2xl leading-7">Luke Smith</p>
-                    <p class="not-italic font-normal text-xs leading-4 text-center text-orange-300">Frontend Developer</p>
-                    <p class="pt-2 not-italic font-normal text-center text-xs text-gray-200">lukesmith.website</p>
+                    <p class="not-italic font-bold text-2xl leading-7" id="person-name"></p>
+                    <p class="not-italic font-normal text-xs leading-4 text-center text-orange-300" id="person-role"></p>
+                    <p class="pt-2 not-italic font-normal text-center text-xs text-gray-200" id="person-website"></p>
                 </div>
                 <div id="contact" class="flex w-[247px] mx-auto pt-5 justify-between">
                     <div id="email-button" class="flex flex-row w-1/2 py-2 mr-3 justify-center items-center bg-white text-gray-900 box-border rounded-md"><FontAwesomeIcon icon={faEnvelope} />  <div class="pl-2">Email</div> </div>
@@ -22,11 +26,11 @@ export default function BusinessCard() {
                 </div>
                 <div id="about" class="text-left w-[247px] mx-auto mt-6">
                     <h1 class=" not-italic font-bold text-base text-gray-200">About</h1>
-                    <p class="pt-1 not-italic font-normal text-gray-400 text-xs">I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn. </p>
+                    <p class="pt-1 not-italic font-normal text-gray-400 text-xs" id="person-about"></p>
                 </div>
                 <div id="interests" class="pt-4 pb-6 text-left w-[247px] mx-auto ">
                     <h1 class="not-italic font-bold text-base text-gray-200">Interests</h1>
-                    <p class="pt-1 not-italic font-normal text-gray-400 text-xs">Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+                    <p class="pt-1 not-italic font-normal text-gray-400 text-xs" id="person-interests"></p>
                 </div>
             </div>
             <div id="social" class="bg-black w-[317px] mx-auto py-3 text-gray-500 text-3xl flex justify-center box-border rounded-b-md">
